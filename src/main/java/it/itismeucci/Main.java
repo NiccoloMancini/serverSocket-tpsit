78677132429bde3de54b8b4c389db01d49de2efb
Niccolo <niccolomancini3@gmail.com>
2024-10-09 09:08:49 +0200

    fine comunicazione server

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,6 +1,9 @@
 package it.itismeucci;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,5 +12,13 @@ public class Main {
         ServerSocket ss = new ServerSocket(5673);
         Socket s = ss.accept();
         System.out.println("qualcuno si è collegato");
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        String stringaRicevuta = in.readLine();
+        System.out.println("la stringa ricevuta: " + stringaRicevuta);
+        String stringaMaiuscola = stringaRicevuta.toUpperCase();
+        out.writeBytes(stringaMaiuscola);
+        s.close();
+        ss.close();
     }
 }
