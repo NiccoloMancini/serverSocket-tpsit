cb1c47ef3ba105a6da2de31048ed6fa6d4567da9
Niccolo <niccolomancini3@gmail.com>
2024-10-09 08:45:31 +0200

    socketServer cerated

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,7 +1,13 @@
 package it.itismeucci;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(5673);
+        Socket s = ss.accept();
+        System.out.println("qualcuno si è collegato");
     }
 }
